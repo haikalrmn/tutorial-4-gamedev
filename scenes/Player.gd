1449bfce4efcfab6eee1af2df69e9a33754c728d
haikal.rahman <haikal.rahman@ui.ac.id>
2024-03-13 20:56:58 +0700

    Finish tutorial 5

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -5,11 +5,13 @@ const UP = Vector2(0, -1)
 export var speed: int = 400
 export var GRAVITY: int = 1200
 export var jump_speed: int = -400
+export var slide_distance: int = 200
+export var slide_speed: int = 50
 
 var velocity: Vector2 = Vector2()
 
 
-func get_input():
+func get_input(_delta):
 	velocity.x = 0
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = jump_speed
@@ -17,12 +19,20 @@ func get_input():
 		velocity.x += speed
 	if Input.is_action_pressed("left"):
 		velocity.x -= speed
+		slide_distance = -slide_distance
+		
+	for i in range(get_slide_count() - 1):
+		var collision = get_slide_collision(i)
+		if collision.collider.name == "SlipperyPlatform":
+			
+			position.x = lerp(position.x, position.x + 1000, _delta)
+	
 
 
 func _physics_process(_delta):
-	velocity.y += delta * GRAVITY
-	get_input()
-	velocity = move_and_slide(velocity, UP)
+	velocity.y += _delta * GRAVITY
+	get_input(_delta)
+	velocity = move_and_slide(velocity, UP, true)
 
 
 func _process(_delta):
---
+++ scenes/Spawner.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+export (PackedScene) var obstacle
+export var spawnTimeMin: float = 0.2
+export var spawnTimeMax: float = 1
+
+func _ready():
+	repeat()
+
+func spawn():
+	var spawned = obstacle.instance()
+	get_parent().add_child(spawned)
+
+	var spawn_pos = global_position
+	spawn_pos.x = spawn_pos.x
+
+	spawned.global_position = spawn_pos
+
+func repeat():
+	spawn()
+	yield(get_tree().create_timer(rand_range(spawnTimeMin, spawnTimeMax)), "timeout")
+	repeat()
